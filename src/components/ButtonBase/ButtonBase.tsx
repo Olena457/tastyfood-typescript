526ff382442ea97fd375d710aaa373a6974a8474
Olena457 <olenademianenko42@gmail.com>
2025-07-19 19:09:19 +0300

    added dependencies

---
+++ src/components/ButtonBase/ButtonBase.tsx
@@ -0,0 +1,16 @@
+// const MyButton = () => {
+//   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+//     console.log("Clicked!", e);
+//   };
+
+//   return (
+//     <button
+//       className=""
+//       onClick={handleClick}
+//       type="button"
+//     >
+//
+//     </button>
+//   );
+// };
+// export default MyButton;
