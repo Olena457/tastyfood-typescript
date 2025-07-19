d3ed5e2550e04207a4aaa3cc3c0205d925a1c411
Olena457 <olenademianenko42@gmail.com>
2025-07-20 01:04:28 +0300

    created tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,14 @@
 // return <ButtonBase />;
 
 const App: React.FC = () => {
-  return <h1> працює!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold  underline text-blue-500">
+        Hello world!
+      </h1>
+      <button className="bg-emerald-400  rounded-4xl w-50">Click me</button>
+    </>
+  );
 };
 
 export default App;
