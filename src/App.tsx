645927f735d85bf2f41b85a809ae940b307aabac
Olena457 <olenademianenko42@gmail.com>
2025-07-20 12:45:32 +0300

    corected styles tailwind preconected fonts

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,23 @@
 const App: React.FC = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold  underline text-blue-500">
-        Hello world!
-      </h1>
-      <button className="bg-emerald-400  rounded-4xl w-50">Click me</button>
+      <div className="bg-blue-500 sm:bg-red-500 lg:bg-amber-900">
+        <div className="card rounded-none">
+          <h1 className="text-3xl font-bold  outline-dashed text-blue-500">
+            Hello world!
+          </h1>
+          <ul>
+            <li>I am first </li>
+            <li>I am second</li>
+            <li>I am third</li>
+          </ul>
+          <p>this a simle text with new fonts</p>
+          <p>this a simle text with two fonts</p>
+
+          <button className="bg-emerald-400  rounded-4xl w-50">Click me</button>
+          <button className="my-custom-button"> No Click </button>
+        </div>
+      </div>
     </>
   );
 };
--- src/components/ButtonBase/ButtonBase.tsx
+++ src/components/ButtonBase/ButtonBase.tsx
@@ -14,3 +14,12 @@
 //   );
 // };
 // export default MyButton;
+// React Example
+// function MyCustomDiv({ children }) {
+//   const classes = "col-start-3 row-start-3 flex max-w-lg flex-col bg-gray-100 p-2 dark:bg-white/10";
+//   return (
+//     <div className={classes}>
+//       {children}
+//     </div>
+//   );
+// }
