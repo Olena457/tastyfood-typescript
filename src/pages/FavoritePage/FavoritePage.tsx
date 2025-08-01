beb7d0980125ec0e4c62c6123e483967a4f9b84a
Olena457 <olenademianenko42@gmail.com>
2025-08-01 12:34:20 +0300

    corected styles page container

--- src/pages/FavoritePage/FavoritePage.tsx
+++ src/pages/FavoritePage/FavoritePage.tsx
@@ -8,7 +8,7 @@ export default function FavoritePage() {
   const favorites = useSelector(selectFavorites);
 
   return (
-    <div className="container  mx-auto px-4 sm:px-6 md:px-8 lg:px-12 xl:px-16 2xl:px-20">
+    <div className="container  mx-auto px-4 sm:px-6 md:px-8 lg:px-12 xl:px-10 2xl:px-9">
       <h1
         className="title font-semibold text-center text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl
                         px-4 py-2 rounded mb-8
